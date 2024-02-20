66bf291c854751b0e6f7e99993b40235d6f84f91
“Ewin” <ewinoaks@gmail.com>
2024-02-20 15:44:23 +0800

    FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,25 @@
-import { View } from "react-native";
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
-//import ProductListItem from "@/components/ProductListItem";
+import { View, FlatList } from "react-native";
+//import products from "../../../assets/data/products";
+import products from "@assets/data/products";
+//import ProductListItem from "@/src/components/ProductListItem";
+import ProductListItem from "@/components/ProductListItem";
 
-//Continue: 31:23
+//Continue: 46:53
 
-// cont 29
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      {/*
+  <ProductListItem product={products[0]} />
+  <ProductListItem product={products[1]} />
+     */}
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   );
 }
